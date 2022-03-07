d1e53de8440769a3c59a7e39c8b40dc4a9cb73ff
sofi12321 <95312480+sofi12321@users.noreply.github.com>
2022-03-07 18:56:02 +0500

    Update ex2
    ex2 update

--- ex02.hpp
+++ ex02.hpp
@@ -2,20 +2,28 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 
+template <int T,float T>
 public class Calculator {
 private:
-    int num1, num2;
+    T num1, num2;
 
 public:
-    Calculator(int n1, int n2) {
+    Calculator(T n1, T n2) {
         num1 = n1;
         num2 = n2;
     }
 
-    int add() { return 0; /*your code here*/ }
-    int subtract() { /*your code here*/ }
-    int multiply() { /*your code here*/ }
-    int divide() { /*your code here*/ }
+    T add() { return num1+num2; /*your code here*/ }
+    T subtract() { 
+        return num1-num2;
+        /*your code here*/ }
+    T multiply() { 
+        return num1*num2;
+        /*your code here*/ }
+    T divide() { 
+        if (num2==0) return 0;
+        return num1/num2;
+        /*your code here*/ }
 };
 
 
