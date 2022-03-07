f204660d33455dce4982cfe4a2dc8ff86d67c383
sofi12321 <95312480+sofi12321@users.noreply.github.com>
2022-03-07 19:08:53 +0500

    ex1 cl

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,7 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <typename WhatKind>
+template <class WhatKind>
 void multiples (WhatKind& sum, WhatKind x, int n)
 {
     sum = 1;
